a248684a4812990432f04647cf1420550d9b1160
user <samuel_morgan_@outlook.com>
2024-12-13 19:54:18 -0500

    new attempt

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import preact from "@preact/preset-vite";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    preact({
-      prerender: {
-        enabled: true,
-        renderTarget: "#app",
-      },
-    }),
-  ],
-  base: "/krashen-tracker/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    plugins: [
+      preact({
+        prerender: {
+          enabled: true,
+          renderTarget: "#app",
+        },
+      }),
+    ],
+    base: "/krashen-tracker/",
+    define: {
+      // For production build, embed env variables
+      ...(mode === "production" && {
+        "import.meta.env.VITE_SUPABASE_URL": JSON.stringify(
+          process.env.VITE_SUPABASE_URL
+        ),
+        "import.meta.env.VITE_SUPABASE_ANON_KEY": JSON.stringify(
+          process.env.VITE_SUPABASE_ANON_KEY
+        ),
+      }),
+    },
+  };
 });
